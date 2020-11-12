d73299ff7e5eef6478952addb804cc2e92973e81
NewWisdom <wlgp2500@gmail.com>
2020-11-12 11:04:17 +0900

    Quiz

--- routes/index.js
+++ routes/index.js
@@ -25,10 +25,7 @@ router.post('/comments', async (req, res, next) => {
 router.get('/users/:id/comments', async (req, res, next) => {
   try {
     const comments = await Comment.findAll({
-      include: {
-        model: User,
-        where: { id: req.params.id },
-      },
+      // 여기를 채워보세요 ~
     });
     console.log(comments);
     res.json(comments);
