8808a94db7243ff46c429bd2dca8a0e3653d94d7
NewWisdom <wlgp2500@gmail.com>
2020-11-12 10:55:06 +0900

    index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST comments. */
+router.post('/comments', async (req, res, next) => {
+  try {
+    const comment = await Comment.create({
+      commenter: req.body.id,
+      comment: req.body.comment,
+    });
+    console.log(comment);
+    res.status(200).json(comment);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+/* GET User's comments. */
 router.get('/users/:id/comments', async (req, res, next) => {
   try {
     const comments = await Comment.findAll({
@@ -22,38 +38,5 @@ router.get('/users/:id/comments', async (req, res, next) => {
     next(err);
   }
 });
-
-//
-router.post('/comments', async (req, res, next) => {
-  try {
-    const comment = await Comment.create({
-      commenter: req.body.id,
-      comment: req.body.comment,
-    });
-    console.log(comment);
-    res.status(201).json(comment);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-// GET /comment/id
-router.get('/comments/:id',function(req,res,next){
-  Comment.findAll({
-      include : {
-          model:User,
-          where:{id:req.params.id},
-      }
-  })
-      .then((comments)=>{
-          console.log(comments);
-          res.json(comments);
-      })
-      .catch((err)=>{
-          console.error(err)
-          next(err)
-      })
-})
 
 module.exports = router;
